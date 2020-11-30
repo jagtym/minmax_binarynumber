01ebc0b485db02e2c1490d2b5d4030fa9ccc13ac
jagtym <jagtymxd@gmail.com>
2020-11-30 11:06:20 +0100

    first

---
+++ find_biggest_binary.py
@@ -0,0 +1,25 @@
+binaries = ["100100010110101101",
+            "00010101011110101",
+            "0000000000000000",
+            "100000010110101101",
+            "100000010110101101",
+            "10000100010110101101",
+            "11001100010110101101",
+            "11001101010110101101",
+            "11000100010110101101"]
+
+
+# creates a list with indexes of all 1's (counting from right to left side)
+def one_pos(input_string):
+    one_indexes = []
+    for idx, digit in enumerate(input_string):
+        if digit == "1":
+            one_indexes.append(len(input_string) - idx)
+    return one_indexes
+
+
+by_one_pos = [one_pos(x) for x in binaries]
+
+# prints the binary with highest 1 indexes and lowest 1 indexes
+print("największa:", binaries[by_one_pos.index(sorted(by_one_pos)[-1])],
+      "najmniejsza:", binaries[by_one_pos.index(sorted(by_one_pos)[0])])
